edcfc9dba49832138fd555f6eccd44a8c002864b
Barnabe <barnabedikartola@gmail.com>
2022-11-15 02:10:47 -0300

    new matrix 53

---
+++ .github/workflows/teste2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+          #frame inicial
+          startframe=0
+          #frame final
+          endframe=3000
+          #numero de divisões do projeto
+          divisor=3
+          #numero total de frames do projeto
+          totalframes=$["$endframe" - "$startframe"]
+          #numero de frames por seguimento 
+          xframe=$["$totalframes" / "$divisor"]
+          #gerar lista com os numeros de frames iniciais e finais
+          for (( x=1; x<="$divisor"; x++ )); do  
+              fend=$["$xframe" * "$x"]
+              fstart=$["$fend" - "$xframe" + "1"]
+              divframes="-s $fstart -e $fend"
+              matrix="${matrix}, $divframes"
+              # echo "matrixx=$matrix" >> $GITHUB_ENV
+          done
+              echo $matrix | sed 's/^, //'
