7576ad7c3267a64165057d7305b5e1bf2d8b9641
Barnabe <barnabedikartola@gmail.com>
2022-11-15 21:47:16 -0300

    muda pra env: v10

--- .github/workflows/teste3.sh
+++ .github/workflows/teste3.sh
@@ -1,28 +1,31 @@
-#!/bin/bash
-#           #frame inicial
-#           startframe=0
-#           #frame final
-#           endframe=3000
-#           #numero de divisões do projeto
-#           divisor=3
-#           #numero total de frames do projeto
-#           totalframes=$["$endframe" - "$startframe"]
-#           #numero de frames por seguimento 
-#           xframe=$["$totalframes" / "$divisor"]
-#           #gerar lista com os numeros de frames iniciais e finais
-#           for (( x=1; x<="$divisor"; x++ )); do  
-#               fend=$["$xframe" * "$x"]
-#               fstart=$["$fend" - "$xframe" + "1"]
-#               divframes="-s $fstart -e $fend"
-#               matrix="${matrix}, $divframes"
-#               # echo "matrixx=$matrix" >> $GITHUB_ENV
-#           done
-#               echo $matrix | sed 's/^, //'
-cd ../..
-totalframes=$(blender -b big-vs-ubuntu.blend -S 03-montagem -P frames.py | grep frames | cut -d "=" -f2)
-startframe=$(blender -b big-vs-ubuntu.blend -S 03-montagem -P frames.py | grep frames | sed 's/^.*frames: //' | cut -d " " -f1 | awk -F\. '{print $1}')
-endframe=$(blender -b big-vs-ubuntu.blend -S 03-montagem -P frames.py | grep frames | sed 's/^.*frames: //' | cut -d " " -f1 | awk -F\. '{print $3}')
-
-echo $totalframes
-echo $startframe
-echo $endframe
+!/bin/bash
+          #frame inicial
+          startframe=50
+          #frame final
+          endframe=3050
+          #numero de divisões do projeto
+          divisor=5
+          #numero total de frames do projeto
+          totalframes=$["$endframe" - "$startframe"] #3000
+          #numero de frames por seguimento 
+          xframe=$["$totalframes" / "$divisor"]
+          #gerar lista com os numeros de frames iniciais e finais
+          for (( x="$divisor"; x>=1; x-- )); do
+              fstart=$["$xframe" * "$x" - "1" - "$endframe"]
+              fend=$["$xframe" - "$fstart" - 1]
+              if [ "$x" = "$divisor" ]; then
+                fstart=$["$fstart" + "1"]
+              fi
+              divframes="-s $fstart -e $fend"
+              matrix="${matrix}, $divframes"
+              # echo "matrixx=$matrix" >> $GITHUB_ENV
+          done
+              echo $matrix | sed 's/^, //'
+# cd ../..
+# totalframes=$(blender -b big-vs-ubuntu.blend -S 03-montagem -P frames.py | grep frames | cut -d "=" -f2)
+# startframe=$(blender -b big-vs-ubuntu.blend -S 03-montagem -P frames.py | grep frames | sed 's/^.*frames: //' | cut -d " " -f1 | awk -F\. '{print $1}')
+# endframe=$(blender -b big-vs-ubuntu.blend -S 03-montagem -P frames.py | grep frames | sed 's/^.*frames: //' | cut -d " " -f1 | awk -F\. '{print $3}')
+# 
+# echo $totalframes
+# echo $startframe
+# echo $endframe
